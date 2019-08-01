dec8062e135f7a8c2e3f1ae63bef310567c4114f
panqinghu <1213618287@qq.com>
2019-08-01 08:55:58 +0800

    2019.8.1 8:55

---
+++ app/Http/Requests/Api/UserRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return [
+            'verification_key' => '短信验证码 key',
+            'verification_code' => '短信验证码',
+        ];
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|between:3,25|unique:users,name',
+            'password' => 'required|string|min:6',
+            'verification_key' => 'required|string',
+            'verification_code' => 'required|string',
+        ];
+    }
+}
